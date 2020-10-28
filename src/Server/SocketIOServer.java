d7ff86ee786e239c066715535afb7691bb5da71d
Jasmina Brar <jasmina.brar@mail.utoronto.ca>
2020-10-27 21:11:52 -0400

    Implemented the SocketIOServer class which contains the working SocketIOServer. Added maven dependencies to 2 libraries needed for the server: netty-socketio and slf4j-simple.

---
+++ src/Server/SocketIOServer.java
@@ -0,0 +1,50 @@
+package Server;
+import com.corundumstudio.socketio.*;
+import com.corundumstudio.socketio.listener.ConnectListener;
+import com.corundumstudio.socketio.listener.DataListener;
+
+public class SocketIOServer {
+    /* This is the Socket IO Server that will connect to the Hypatia app and recieve
+    the emitted events
+
+    SocketIO is a library that allows for bi-directional communication between clients and
+    servers. We are using the server-side library to implement this SocketIO server. SocketIO
+    uses Websocket protocol. We must communicate over this protocol because the Hypatia app does.
+    The SocketIO library will handle the Websocket Handshake for us upon connecting with Hypatia.
+
+     */
+
+    public static void main(String[] args) throws InterruptedException{
+        // configure this server to listen as localhost on port 3333
+        Configuration config = new Configuration();
+        config.setHostname("localhost");
+        config.setPort(3333);
+        com.corundumstudio.socketio.SocketIOServer server = new com.corundumstudio.socketio.SocketIOServer(config);
+
+        // once Hypatia is connected to the server this method will be run
+        server.addConnectListener(new ConnectListener() {
+            @Override
+            public void onConnect(SocketIOClient socketIOClient) {
+                System.out.println("Connected to Hypatia's CheckMath");
+            }
+        });
+
+        // listen for the "expressions" event emitted by Hypatia
+        server.addEventListener("expressions", String.class, new DataListener<String>() {
+            @Override
+            public void onData(SocketIOClient socketIOClient, String s, AckRequest ackRequest) throws Exception {
+                System.out.println("Client Expression" + s);
+            }
+        });
+
+        // listen for the "results" event emitted by Hypatia
+        server.addEventListener("result", String.class, new DataListener<String>() {
+            @Override
+            public void onData(SocketIOClient socketIOClient, String s, AckRequest ackRequest) throws Exception {
+                System.out.println("Client Results" + s);
+            }
+        });
+
+        server.start();
+    }
+}
