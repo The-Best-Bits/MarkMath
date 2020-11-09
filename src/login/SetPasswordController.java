ddb4ca6fcf973e054753b0f05cbb39b85721d564
Shilei Xu <shilei.xu@mail.utoronto.ca>
2020-11-09 13:49:16 -0500

    Added set password page.

---
+++ src/login/SetPasswordController.java
@@ -0,0 +1,23 @@
+package login;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class SetPasswordController {
+
+    @FXML
+    private JFXButton passwordButton;
+
+    @FXML
+    private JFXPasswordField password;
+
+    //TODO: Store password into the database and return to the sign in page.
+    @FXML
+    void setPassword(ActionEvent event) {
+
+    }
+
+}
