ef95e5ae7e5393d29bbb2d542d9ed4783832a637
Shilei Xu <shilei.xu@mail.utoronto.ca>
2020-11-29 21:01:21 -0500

    Help pages for Dashboard and Classroom. Minor visual changes.

---
+++ src/Classroom/howtoController.java
@@ -0,0 +1,16 @@
+package Classroom;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class howtoController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
